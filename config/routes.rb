698ca16d57730e2a2c9ac6a4e3ac4d31d64039fd
ta1kt0me <p.wadachi@gmail.com>
2016-02-16 23:17:48 +0900

    /user/users/

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'user/new'
+  get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UserControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get user_new_url
     assert_response :success
