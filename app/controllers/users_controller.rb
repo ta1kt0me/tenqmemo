ea0e758490c2ee5ab6bf9f1e2fe65c71c6cb356e
ta1kt0me <p.wadachi@gmail.com>
2016-02-16 23:32:10 +0900

    User new & create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = 'Welcome to tenqmemo'
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
