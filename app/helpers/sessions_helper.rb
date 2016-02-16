0a73ee051196805de77261e13e798f671f18659f
ta1kt0me <p.wadachi@gmail.com>
2016-02-16 23:58:28 +0900

    Utility method for session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,13 @@
 module SessionsHelper
   def log_in(user)
-    session[:current_user] = user
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !current_user.nil?
   end
 end
