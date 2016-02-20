e8543a38a6e1650c9a6cccb2e66c754efc6d44be
ta1kt0me <p.wadachi@gmail.com>
2016-02-21 00:50:40 +0900

    Deny to create new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :notes
   resources :sessions, only: [:new, :create]
-  resources :users, only: [:new, :create]
+  # resources :users, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
