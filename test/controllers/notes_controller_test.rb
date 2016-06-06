bcea8e2f155d5857938fbbf82cb57a674621e602
ta1kt0me <p.wadachi@gmail.com>
2016-06-07 00:35:08 +0900

    Fix login in data

--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -1,48 +1,56 @@
 require 'test_helper'
 
 class NotesControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @note = notes(:one)
-  end
-
   test 'should get index' do
     get notes_url
     assert_response :success
   end
 
   test 'should get new' do
-    get new_note_url
-    assert_response :success
+    sign_in_user do
+      get new_note_url
+      assert_response :success
+    end
   end
 
   test 'should create note' do
-    assert_difference('Note.count') do
-      post notes_url, params: { note: { body: @note.body, user_id: @note.user_id } }
-    end
+    sign_in_user do
+      assert_difference('Note.count') do
+        post notes_url, params: { note: { body: 'note body' } }
+      end
 
-    assert_redirected_to note_path(Note.last)
+      assert_redirected_to note_path(Note.last)
+    end
   end
 
   test 'should show note' do
+    @note = notes(:one)
     get note_url(@note)
     assert_response :success
   end
 
   test 'should get edit' do
-    get edit_note_url(@note)
-    assert_response :success
+    sign_in_user do
+      @note = notes(:one)
+      get edit_note_url(@note)
+      assert_response :success
+    end
   end
 
   test 'should update note' do
-    patch note_url(@note), params: { note: { body: @note.body, user_id: @note.user_id } }
-    assert_redirected_to note_path(@note)
+    sign_in_user do
+      @note = notes(:one)
+      patch note_url(@note), params: { note: { body: 'note body' } }
+      assert_redirected_to note_path(@note)
+    end
   end
 
   test 'should destroy note' do
-    assert_difference('Note.count', -1) do
-      delete note_url(@note)
-    end
+    sign_in_user do
+      @note = notes(:one)
+      assert_difference('Note.count', -1) { delete note_url(@note) }
 
-    assert_redirected_to notes_path
+      assert_redirected_to notes_path
+    end
   end
 end
