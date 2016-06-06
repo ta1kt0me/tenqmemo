8382ce23d945648faabb850f61f86ff675a142ed
ta1kt0me <p.wadachi@gmail.com>
2016-06-07 00:40:36 +0900

    Fix no routing error test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test 'should get new' do
-    get user_new_url
+    get new_user_url
     assert_response :success
   end
 end
