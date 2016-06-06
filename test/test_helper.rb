a08fa2963248f0073bd40e8e44193065ab033653
ta1kt0me <p.wadachi@gmail.com>
2016-06-07 00:34:32 +0900

    Fix syntax for nested module

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,12 @@ require 'rails/test_help'
 require 'minitest/stub_any_instance'
 require 'support/login_helper'
 
-class ActiveSupport::TestCase
-  include LoginHelper
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+module ActiveSupport
+  class TestCase
+    include LoginHelper
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+    # Add more helper methods to be used by all tests here...
+  end
 end
