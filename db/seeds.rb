9c0664fa6d12a90d11300bea62e4f514789d5c73
ta1kt0me <p.wadachi@gmail.com>
2016-12-13 01:02:17 +0900

    Setup seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.find_or_initialize_by({ username: 'foo', email: 'foo@example.com',  }) do |u|
+  u.password = 'test'*3
+  u.password_confirmation = 'test'*3
+end
+
+data = Array.new(300) { |i| { body: ('A'..'z').to_a.join, user_id: user.id } }
+Note.create data
