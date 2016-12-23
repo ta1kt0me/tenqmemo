de205e6ff115143c4e13d41ddf016e26a5f441bb
ta1kt0me <p.wadachi@gmail.com>
2016-12-24 08:23:14 +0900

    Useless require

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( emoji/**/*.png )
