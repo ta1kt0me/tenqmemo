765103b06a959c3b7e51af149560e8e857a0a814
ta1kt0me <p.wadachi@gmail.com>
2017-03-12 00:36:09 +0900

    Manage bootstrap via webpack

--- app/frontend/javascripts/entry.js
+++ app/frontend/javascripts/entry.js
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap-material-design/dist/js/material';
+import 'bootstrap-material-design/dist/js/ripples';
 import { Editor } from './components/editor.js';
 import { Preview } from './components/preview.js';
 import { Tag } from './components/tag.js';
