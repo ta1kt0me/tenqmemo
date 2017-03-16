ff737e0f238d11ea72da90331177e7d9174dddd0
ta1kt0me <p.wadachi@gmail.com>
2017-03-17 02:23:51 +0900

    Refactor app stylesheets

--- app/frontend/entry.js
+++ app/frontend/entry.js
@@ -16,7 +16,4 @@ import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 import 'bootstrap-material-design/sass/bootstrap-material-design.scss';
 import 'bootstrap-material-design/sass/ripples.scss';
 
-import './stylesheets/common.scss';
-import './stylesheets/notes.scss';
-import './stylesheets/tags.scss';
-import './stylesheets/suggestions.scss';
+import './stylesheets/app.scss';
