09081cb0b6d6c01f1cfd45cc4f44e19c7312a90f
ta1kt0me <p.wadachi@gmail.com>
2017-03-17 00:21:48 +0900

    Add test for tag link

---
+++ test/features/can_access_notes_from_tag_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class CanAccessHomeTest < Capybara::Rails::TestCase
+  test 'click a tag link' do
+    visit root_path
+    click_link 'pop music'
+    within '.list-group-item-text' do
+      assert_content notes(:one).body
+    end
+  end
+end
