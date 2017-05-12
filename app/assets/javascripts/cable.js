88d5590e0edd2c9da1906fa52781647e09094229
ta1kt0me <p.wadachi@gmail.com>
2017-05-13 00:04:22 +0900

    Remove coffeescript

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -1,11 +1,13 @@
 // Action Cable provides the framework to deal with WebSockets in Rails.
-// You can generate new channels where WebSocket features live using the rails generate channel command.
-//
-// Turn on the cable connection by removing the comments after the require statements (and ensure it's also on in config/routes.rb).
+// You can generate new channels where WebSocket features live using the `rails generate channel` command.
 //
 //= require action_cable
 //= require_self
 //= require_tree ./channels
-//
-this.App || (this.App = {});
-App.cable = ActionCable.createConsumer();
+
+(function() {
+  this.App || (this.App = {});
+
+  App.cable = ActionCable.createConsumer();
+
+}).call(this);
