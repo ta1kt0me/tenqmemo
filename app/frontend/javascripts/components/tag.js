4b4261f6ce3d3807cd52e1f5202e9ef47a656778
ta1kt0me <p.wadachi@gmail.com>
2017-11-28 22:48:41 +0900

    Stylish

--- app/frontend/javascripts/components/tag.js
+++ app/frontend/javascripts/components/tag.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { WithContext as ReactTags } from 'react-tag-input';
