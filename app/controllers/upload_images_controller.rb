50e491e56360586a39835683c07cc5f6835c4e39
ta1kt0me <p.wadachi@gmail.com>
2017-12-03 13:51:16 +0900

    Simplify

--- app/controllers/upload_images_controller.rb
+++ app/controllers/upload_images_controller.rb
@@ -5,17 +5,17 @@ class UploadImagesController < ApplicationController
 
   def create
     respond_to do |format|
-      @upload_image = upload_image_params
+      @image = upload_image_params.dig :file
       begin
         res = Cloudinary::Uploader.upload(
-          @upload_image[:file],
+          @image,
           crop: "limit",
           tags: "tenqmemo",
           resource_type: :auto,
           folder: "tenqmemo",
           use_filename: true
         )
-        file = { name: @upload_image[:file].original_filename, url: res["secure_url"] }
+        file = { name: @image.original_filename, url: res["secure_url"] }
         format.json { render json: file }
       rescue CloudinaryException => e
         format.json { render json: e.messsge, status: :unprocessable_entity }
