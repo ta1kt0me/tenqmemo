2206213d4170f6ab41f0ff60dc7039cf718142f1
ta1kt0me <p.wadachi@gmail.com>
2018-04-10 23:51:19 +0900

    Simplify test setting

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,17 +1,5 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  Capybara.javascript_driver = :selenium
-  driven_by(
-    :selenium,
-    using: :chrome,
-    screen_size: [1400, 900],
-    options: {
-      desired_capabilities: {
-        chromeOptions: {
-          args: %w[headless disable-gpu]
-        }
-      }
-    }
-  )
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 end
